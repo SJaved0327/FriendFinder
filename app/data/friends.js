b9d84dbc0d17041d1d6fb86ef3d4fdf8353be9d2
Sara Javed <sarajaved@Saras-Air.fios-router.home>
2017-11-17 22:06:03 -0500

    routes are working

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,11 @@
+var friendsArray = [
+  {
+    Name: "Sara",
+    Photo: "http://cdn3-www.dogtime.com/assets/uploads/gallery/french-bulldog-dog-breed-pictures/9-face.jpg",
+    Answers: ["1", "2", "3", "4", "5", "1", "2", "3", "4", "5"]
+  }
+];
 
+module.exports = friendsArray;
 
 
