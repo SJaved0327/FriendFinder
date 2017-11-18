edf9741628ba18042b588a660620d781c44e399e
Sara Javed <sarajaved@Saras-Air.fios-router.home>
2017-11-17 20:56:22 -0500

    questions populating correctly with correct data attributes

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
-const app = require(`express`);
-const bodyparser = require(`body-parser`);
+const express = require(`express`);
+const bodyParser = require(`body-parser`);
 const path = require(`path`);
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// ================================================================================
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// =============================================================================
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
